e4631eb8c59c192bb3ec55a78e00bb88f5a78738
Helios He <helioshe4@gmail.com>
2023-07-04 22:43:39 -0400

    reorganized, added some docs

--- include/api.h
+++ include/api.h
@@ -9,40 +9,22 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 #include "workoutdatapoint.h"
+#include <time.h>
 
-/*
-struct WorkoutDataPoint {
-  std::string name;
-  double distance;
-  int moving_time;
-  int elapsed_time;
-  double total_elevation_gain;
-  std::string type;
-  std::string sport_type;
-  std::string workout_type;
-  int id;
-  std::string start_date;
-  std::string start_date_local;
-  std::string timezone;
-  std::string location_city;
-  std::string location_state;
-  std::string location_country;
 
-  // todo decide which other data points to add
-  double average_speed;
-  double max_speed;
-  //bool has_heartrate;
-  double average_heartrate;
-  double max_heartrate;
-  double elev_high;
-  double elev_low;
-};
-*/
-
+// Converts json response from strava api to vector of workout points
 std::vector<WorkoutDataPoint> convertJsonToVector(const nlohmann::json &jsonArray);
+
+// loads env variables from .env
 void loadEnvFromFile(const std::string &filename);
+
+// handles json response converts to usable format
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *s);
+
+// gets json response from strava api
 nlohmann::json getJsonFromUrl(const std::string &url, const std::string &header, const std::string &postFields = "");
+
+// wrapper that returns all the workouts from user
 std::vector<WorkoutDataPoint> getWorkoutData();
 
 #endif
