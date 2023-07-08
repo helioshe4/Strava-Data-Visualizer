82dc9e615c91765b455ae34993b6fc6820dcd7b3
Helios He <helioshe4@gmail.com>
2023-07-08 18:43:30 -0400

    fixed y axis issue

--- QTStravaChart/main.cpp
+++ QTStravaChart/main.cpp
@@ -77,17 +77,16 @@ int main(int argc, char *argv[])
     }
     */
 
-
-    QLineSeries *series1 = new QLineSeries();
-    QLineSeries *series2 = new QLineSeries();
-    QLineSeries *series3 = new QLineSeries();
-    QScatterSeries *series4 = new QScatterSeries();
+    QLineSeries *series_distance = new QLineSeries();
+    QLineSeries *series_avg_hr = new QLineSeries();
+    QLineSeries *series_moving_time = new QLineSeries();
+    QScatterSeries *series_scatter = new QScatterSeries();
 
     std::map<qint64, qreal> maxValues1;
     std::map<qint64, qreal> maxValues2;
     std::map<qint64, qreal> maxValues3;
 
-    qint64 interval = 6 * 60 * 60 * 1000;  // 1 hours in milliseconds
+    qint64 interval = 6 * 60 * 60 * 1000;  // 6 hours in milliseconds
 
     for (const WorkoutDataPoint& dataPoint : data) {
         // Convert the date to a QDateTime
@@ -108,27 +107,28 @@ int main(int argc, char *argv[])
     }
 
     // Now add the points to the series
-    appendPointsToSeries(series1, maxValues1);
-    appendPointsToSeries(series2, maxValues2);
-    appendPointsToSeries(series3, maxValues3);
+    appendPointsToSeries(series_distance, maxValues1);
+    appendPointsToSeries(series_avg_hr, maxValues2);
+    appendPointsToSeries(series_moving_time, maxValues3);
 
     // For the scatter series, just append the points directly
     for (const WorkoutDataPoint& dataPoint : data) {
-        series4->append(dataPoint.average_heartrate, (dataPoint.distance) / 1000);
+        series_scatter->append(dataPoint.average_heartrate, (dataPoint.distance) / 1000);
     }
-
 
     QChart *chart1 = new QChart();
     //series
-    series1->setName("Distance (km)");
-    series2->setName("Average Heart rate (bpm)");
-    series3->setName("Moving Time (min)");
+    chart1->addSeries(series_distance);
+    chart1->addSeries(series_avg_hr);
+    chart1->addSeries(series_moving_time);
+    series_distance->setName("Distance (km)");
+    series_avg_hr->setName("Average Heart Rate (bpm)");
+    series_moving_time->setName("Moving Time (min)");
 
-    chart1->addSeries(series1);
-    chart1->addSeries(series2);
-    chart1->addSeries(series3);
+
 
     //labels on series
+    /*
     series1->setPointLabelsVisible(true);
     series1->setPointLabelsFormat("@yPoint");
 
@@ -137,10 +137,10 @@ int main(int argc, char *argv[])
 
     series3->setPointLabelsVisible(true);
     series3->setPointLabelsFormat("@yPoint");
+    */
 
     chart1->setAnimationOptions(QChart::AllAnimations);
-
-    chart1->setTitle("Workout Distance Over Time");
+    chart1->setTitle("Workout Metrics Over Time");
 
     // Create and set the x-axis
     QDateTimeAxis *axisX = new QDateTimeAxis;
@@ -148,28 +148,40 @@ int main(int argc, char *argv[])
     axisX->setFormat("MMM yyyy");
     axisX->setTitleText("Date");
     chart1->addAxis(axisX, Qt::AlignBottom);
-    series1->attachAxis(axisX);
+
 
     QValueAxis *axisY = new QValueAxis;
     axisY->setLabelFormat("%i");
     axisY->setTitleText("Distance (km)");
-    chart1->addAxis(axisY, Qt::AlignLeft);
-    series1->attachAxis(axisY);
+    chart1->addAxis(axisY, Qt::AlignRight);
 
     QValueAxis *axisY2 = new QValueAxis;
     axisY2->setLabelFormat("%i");
     axisY2->setTitleText("Average HR (bpm)");
     chart1->addAxis(axisY2, Qt::AlignRight);
-    series2->attachAxis(axisY2);
+
+    QValueAxis *axisY3 = new QValueAxis;
+    axisY3->setLabelFormat("%i");
+    axisY3->setTitleText("Moving Time (min)");
+    chart1->addAxis(axisY3, Qt::AlignRight);
+
+    //attaching axes
+    series_distance->attachAxis(axisX);
+    series_avg_hr->attachAxis(axisX);
+    series_moving_time->attachAxis(axisX);
+
+    series_distance->attachAxis(axisY);
+    series_avg_hr->attachAxis(axisY2);
+    series_moving_time->attachAxis(axisY3);
+
 
     //legend
     chart1->legend()->setVisible(true);
     chart1->legend()->setAlignment(Qt::AlignBottom);
 
-\
-    CustomChartView *chartView1 = new CustomChartView(chart1);
+    //customchartview for tooltip
+    QChartView *chartView1 = new QChartView(chart1);
     chartView1->setRenderHint(QPainter::Antialiasing);
-
 
     //chart 2
     QChart *chart2 = new QChart();
@@ -177,24 +189,24 @@ int main(int argc, char *argv[])
     chart2->setTitle("Correlation between average HR and distance");
     chart2->createDefaultAxes();
 
-    series4->setName("HR and Distance");
-    series4->setMarkerShape(QScatterSeries::MarkerShapeCircle);
-    series4->setMarkerSize(15.0);
+    series_scatter->setName("HR and Distance");
+    series_scatter->setMarkerShape(QScatterSeries::MarkerShapeCircle);
+    series_scatter->setMarkerSize(15.0);
 
-    chart2->addSeries(series4);
+    chart2->addSeries(series_scatter);
 
     //axes
     QValueAxis *scatterAxisX = new QValueAxis;
     scatterAxisX->setLabelFormat("%i");
     scatterAxisX->setTitleText("Average HR");
     chart2->addAxis(scatterAxisX, Qt::AlignBottom);
-    series4->attachAxis(scatterAxisX);
+    series_scatter->attachAxis(scatterAxisX);
 
     QValueAxis *scatterAxisY = new QValueAxis;
     scatterAxisY->setLabelFormat("%i");
     scatterAxisY->setTitleText("Distance (km)");
     chart2->addAxis(scatterAxisY, Qt::AlignLeft);
-    series4->attachAxis(scatterAxisY);
+    series_scatter->attachAxis(scatterAxisY);
 
     CustomChartView *chartView2 = new CustomChartView(chart2);
     chartView2->setRenderHint(QPainter::Antialiasing);
